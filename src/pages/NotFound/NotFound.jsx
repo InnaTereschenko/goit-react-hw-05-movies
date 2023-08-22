d8b5bf87a3546488875c7c7734020e9b3e1cabde
InnaTereschenko <120989288+InnaTereschenko@users.noreply.github.com>
2023-08-23 02:13:11 +0300

    add NotFoud file

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,7 @@
- const NotFound = () => {
-    
-}
+import React from 'react';
+
+const NotFound = () => (
+  <img width="500" src="https://http.cat/404" alt="Error 404. Not Found" />
+);
 
 export default NotFound;
